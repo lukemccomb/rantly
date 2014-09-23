8f8fc2c017dc7545d4aa8188df5d2a702f0e8527
Luke McComb <lucasmccomb@gmail.com>
2014-09-22 21:23:00 -0600

    registration view test written and half of form created

--- spec/feature/main_spec.rb
+++ spec/feature/main_spec.rb
@@ -2,10 +2,17 @@ require 'rails_helper'
 require 'capybara/rails'
 
 feature "index page" do
+  before :each do
+    visit '/'
+  end
 
   scenario "anonymous can visit the homepage and see sample quote" do
-    visit '/'
     expect(page).to have_content("Rantly Let it all out")
+  end
+
+  scenario "anonymous can register" do
+    click_on "JOIN"
+    expect(page).to have_content("Register Username Password First name Last name")
   end
 
 end
