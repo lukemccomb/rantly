fbccbe3bce8357c6fa62d3a79e918d7843abd76d
Luke McComb <lucasmccomb@gmail.com>
2014-10-04 16:24:21 -0600

    Logout spec passing

--- spec/feature/main_spec.rb
+++ spec/feature/main_spec.rb
@@ -10,7 +10,7 @@ feature "index page" do
     expect(page).to have_content("Rantly Let it all out")
   end
 
-  scenario "anonymous can register" do
+  before :each do
     click_on "JOIN"
     expect(page).to have_content("Register Username Password First name Last name")
     fill_in "Username", with: "user"
@@ -20,24 +20,27 @@ feature "index page" do
     fill_in "Bio", with: "sample bio"
     choose('Weekly')
     click_on "REGISTER"
+  end
+
+  scenario "anonymous can register" do
     expect(page).to have_content("Thank you for registering! Please log in to begin ranting.")
   end
 
   scenario "Registered user can login" do
-    click_on "JOIN"
-    expect(page).to have_content("Register Username Password First name Last name")
-    fill_in "Username", with: "user"
-    fill_in "Password", with: "password"
-    fill_in "First name", with: "sample"
-    fill_in "Last name", with: "user"
-    fill_in "Bio", with: "sample bio"
-    choose('Weekly')
-    click_on "REGISTER"
     click_on "Login"
     fill_in "Username", with: "user"
     fill_in "Password", with: "password"
     click_on "LOGIN"
     expect(page).to have_content("Welcome sample user")
+  end
+
+  scenario "Logged in user can log out" do
+    click_on "Login"
+    fill_in "Username", with: "user"
+    fill_in "Password", with: "password"
+    click_on "LOGIN"
+    click_on "LOGOUT"
+    expect(page).to have_content("Rantly Let it all out")
   end
 
 end
