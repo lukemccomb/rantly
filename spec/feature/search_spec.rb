f479d741dd43b532b369754cd0f42292afbebb72
Luke McComb <lucasmccomb@gmail.com>
2014-10-30 10:31:23 -0600

    all feature tests are passing

--- spec/feature/search_spec.rb
+++ spec/feature/search_spec.rb
@@ -13,14 +13,21 @@ feature "dashboard" do
     register_user(user)
 
     user2 = {}
-
-
     user.map { |key, value| user2[key] = value + "2" }
     user2[:rate] = "Weekly"
+    user3 = {username: 'Billybob',
+            password: 'password',
+            first_name: 'Billy',
+            last_name: 'Bob',
+            bio: 'Billy Bob is just a guy.',
+            rate: 'Weekly'}
 
     @rant = "Sometimes I order a beet salad, so when the waiter comes and
                             lays down my salad I can say 'thanks for laying down those
                             funky beets'. It's an expensive joke because I don't even like beets."
+
+    @hodor_rant = "hodor hodor hodor hodor hodor hodor hodor hodor hodor hodor
+                    hodor hodor hodor hodor hodor hodor hodor hodor hodor hodor #hodor"
 
     fill_in "A rant about", with: "first rant"
     fill_in "Rant", with: @rant
@@ -28,6 +35,14 @@ feature "dashboard" do
     click_on "LOGOUT"
 
     register_user(user2)
+
+    click_on "LOGOUT"
+
+    register_user(user3)
+
+    fill_in "A rant about", with: "Musings"
+    fill_in "Rant", with: @hodor_rant
+    click_on "RANT"
   end
 
   scenario "user can search for rants by text" do
@@ -38,5 +53,21 @@ feature "dashboard" do
     fill_in "search_term", with: "first"
     click_on "SEARCH"
     expect(page).to have_content(@rant)
+  end
+
+  scenario "Search for names, text and hashtag" do
+    click_on "Search"
+    fill_in "search_term", with: "hodor"
+    click_on "SEARCH"
+    expect(page).to have_content(@hodor_rant)
+    fill_in "search_term", with: "billy"
+    click_on "SEARCH"
+    expect(page).to have_content(@hodor_rant)
+    fill_in "search_term", with: "bob"
+    click_on "SEARCH"
+    expect(page).to have_content(@hodor_rant)
+    fill_in "search_term", with: "billybob"
+    click_on "SEARCH"
+    expect(page).to have_content(@hodor_rant)
   end
 end
