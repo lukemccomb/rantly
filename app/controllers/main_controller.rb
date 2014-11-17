4f5af1b788634935b82fbb389bba06275b134a7b
Luke McComb <lucasmccomb@gmail.com>
2014-11-17 16:46:06 -0700

    if anon visits page after visiting page before without registering, anon sees message saying 'Welcome back, please join'

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,11 @@ class MainController < ApplicationController
   before_filter :check_logged_in_user
 
   def index
+    if flash.empty? && session.delete(:visited)
+      flash[:notice] = "Welcome back! If you like to rant, you should join."
+    end
 
+    session[:visited] = true
   end
 
   private
