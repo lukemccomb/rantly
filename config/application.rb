057acffe9af94ed4f2f6ff97c3994258beb80351
Luke McComb <lemccomb@us.ibm.com>
2017-07-09 00:27:39 -0600

    Changes configs

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module Rantly
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.web_console.development_only = false
   end
 end
